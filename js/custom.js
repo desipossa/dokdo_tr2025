5b6a470b7a8e14e1c86cc4c13ce32682b2db1d84
desipossa <c1jang@hanmail.net>
2025-02-27 14:38:16 +0900

    20250227

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,24 @@ $(function () {
         speed: 800,
         parallax: true,
 
+        navigation: {
+            nextEl: '.main_visual .arrows .next',
+            prevEl: '.main_visual .arrows .prev',
+        },
+
     })
 
+});
+
+
+$(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+
+        if (sct > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    })
 })
