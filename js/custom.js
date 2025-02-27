5c3fc32eebbc97960e5a30954ddfe9d5790ef23f
desipossa <c1jang@hanmail.net>
2025-02-27 13:01:09 +0900

    20250227

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    const MVS = new Swiper('.main_visual .main_visual_slide', {
+        loop: true,
+        grabCursor: true,
+        speed: 800,
+        parallax: true,
+
+    })
+
+})
